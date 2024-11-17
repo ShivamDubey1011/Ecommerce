726b3800225d6e04bcdb0c4a4d003f90652c5fb5
ShivamDubey1011 <dubeyshivam613@gmail.com>
2024-11-17 23:07:53 +0530

    first commit

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ module.exports = {
   jwtSecret: process.env.JWT_SECRET || '622AF2AC23EB47E2F257C546EE4B5',
   mongoURI:
     process.env.MONGO_URI ||
-    'mongodb+srv://dubeysiddhesh613:NDv0tCvNOWyM8R2a@cluster0.dtfzw.mongodb.net/e-commerce',
+    'mongodb+srv://dubeysiddhesh613:NDv0tCvNOWyM8R2a@cluster0.dtfzw.mongodb.net/e-commerce' ,
 };
