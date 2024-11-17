7997fc2d2af5a5c4ae131728ecaa413419687c87
ShivamDubey1011 <dubeyshivam613@gmail.com>
2024-11-17 23:28:55 +0530

    commented userReoutes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
+// app.use('/api/user', userRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
